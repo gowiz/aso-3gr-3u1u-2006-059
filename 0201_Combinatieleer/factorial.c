a4c886e1567aed8f5d6886dda201e04eaec64241
Pieter Pareit <pieter.pareit@gmail.com>
2018-12-30 11:55:13 +0100

    Combinatieleer afwerken

---
+++ 0201_Combinatieleer/factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int factorial(int n) {
+  if (n <= 0) return 1;
+  else return n * factorial(n-1);
+}
+
+int main()
+{
+  char input[128];
+  while (1) {
+    fgets(input, 128, stdin);
+    int n = atoi(input);
+    printf("%d\n", factorial(n));
+  }  
+  return EXIT_SUCCESS;
+}
+
